ffb8998cbbfffed4b05b042ec29ec6682924a103
Zeeshan <zeeshan107526@gmail.com>
2025-03-10 12:41:34 +0500

    Add build folder

---
+++ frontend/build/precache-manifest.58d65e2e24dce5bae59031192a143f06.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d1518041c86332fd2f024612060559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20200e8f10ae798cd473",
+    "url": "/static/css/2.583e7d31.chunk.css"
+  },
+  {
+    "revision": "f5e4f1b06670911944f4",
+    "url": "/static/css/main.2c0ad90e.chunk.css"
+  },
+  {
+    "revision": "20200e8f10ae798cd473",
+    "url": "/static/js/2.e7672512.chunk.js"
+  },
+  {
+    "revision": "417abcc1fc24a9a0f158",
+    "url": "/static/js/3.f66cb13d.chunk.js"
+  },
+  {
+    "revision": "f5e4f1b06670911944f4",
+    "url": "/static/js/main.e1eaf6d3.chunk.js"
+  },
+  {
+    "revision": "331450fd0974b830a155",
+    "url": "/static/js/runtime~main.ff1f67b4.js"
+  },
+  {
+    "revision": "2e303ac36777658ff8d9649d28a76dc0",
+    "url": "/static/media/Digitalize-scaled.2e303ac3.webp"
+  },
+  {
+    "revision": "1ee45197d384e1df7ccbb5d0d19acd22",
+    "url": "/static/media/breadcrums.1ee45197.jpg"
+  },
+  {
+    "revision": "9feb25ff90b21fcbdb54afd51fc8c743",
+    "url": "/static/media/line-vector-sec.9feb25ff.svg"
+  },
+  {
+    "revision": "dc1c195fb29d8703f4a3d84b8df92683",
+    "url": "/static/media/logo.dc1c195f.webp"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
